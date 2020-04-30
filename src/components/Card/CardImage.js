03b9cf1fd681e199900027ff4dd0442fc5a4b0f5
kchia <kchia87@gmail.com>
2020-04-30 14:13:07 -0400

    fix

--- src/components/Card/CardImage.js
+++ src/components/Card/CardImage.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import CardImg from 'react-bootstrap/CardImg';
 import Posters from '../../imgs/posters/Posters';
+import images from '../../imgs/posters/imagesUrl';
 
 function CardImage(props) {
-	let moviePoster = Posters.find((poster) => props.movieId === poster.id);
+  let moviePoster = Posters.find((poster) => props.movieId === poster.id);
+  let image = images.find((image) => props.movieId === image.id);
 
-	console.log(moviePoster);
+  console.log(moviePoster, images);
 
-	return (
-		<div>
-			<CardImg
-				className='img-fluid'
-				variant='top'
-				src={moviePoster.poster}
-				key={moviePoster.title}
-				alt={moviePoster.title}
-			/>
-			{/* <img src={Image} alt='movie poster' /> */}
-		</div>
-	);
+  if (!moviePoster || !image) return null;
+
+  return (
+    <div>
+      <CardImg
+        className="img-fluid"
+        variant="top"
+        src={image.poster}
+        key={moviePoster.title}
+        alt={moviePoster.title}
+      />
+      {/* <img src={Image} alt='movie poster' /> */}
+    </div>
+  );
 }
 
 export default CardImage;
---
+++ src/imgs/posters/imagesUrl.js
@@ -0,0 +1,34 @@
+import castleinthesky from '../../imgs/posters/castleinthesky.jpg';
+import graveofthefireflies from '../../imgs/posters/graveofthefireflies.jpg';
+import myneighbortotoro from '../../imgs/posters/myneighbortotoro.jpg';
+import kikisdeliveryservice from '../../imgs/posters/kikisdeliveryservice.jpg';
+import onlyyesterday from '../../imgs/posters/onlyyesterday.jpg';
+// import image from '../../imgs/posters/porcorosso.jpg';
+// import image from '../../imgs/posters/pompoko.jpg';
+// import image from '../../imgs/posters/whisperoftheheart.jpg';
+// import image from '../../imgs/posters/princessmononoke.jpg';
+// import image from '../../imgs/posters/myneighborstheyamadas.jpg';
+// import image from '../../imgs/posters/spiritedaway.jpg';
+// import image from '../../imgs/posters/thecatreturns.jpg';
+// import image from '../../imgs/posters/howlsmovingcastle.jpg';
+// import image from '../../imgs/posters/talesofearthsea.jpg';
+// import image from '../../imgs/posters/ponyo.jpg';
+// import image from '../../imgs/posters/arrietty.jpg';
+// import image from '../../imgs/posters/fromuponpoppyhill.jpg';
+// import image from '../../imgs/posters/thewindrises.jpg';
+// import image from '../../imgs/posters/thetaleoftheprincesskaguya.jpg';
+// import image from '../../imgs/posters/whenmarniewasthere.jpg';
+
+export default [
+  {
+    id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
+    poster: castleinthesky,
+  },
+  {
+    id: '12cfb892-aac0-4c5b-94af-521852e46d6a',
+    poster: graveofthefireflies,
+  },
+  myneighbortotoro,
+  kikisdeliveryservice,
+  onlyyesterday,
+];
